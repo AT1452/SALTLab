876c5e35a31cf99529ecade9c2847dd610a46ca5
AT1452 <athosar1452@gmail.com>
2021-05-26 01:05:28 -0500

    done graph sorta

--- api/visualize.py
+++ api/visualize.py
@@ -1,3 +1,4 @@
+from os import makedirs
 import pandas as pd
 import matplotlib.pyplot as plt 
 from matplotlib.colors import hsv_to_rgb
@@ -14,44 +15,55 @@ data.plot(x='frames_ids')
 plt.figure(figsize=(10, 8))
 #  xy 
 x, y = data['valence'], data['arousal']
-plt.scatter(x, y)
-plt.xlabel('Valence'); plt.ylabel('Arousal')
+# plt.scatter(x, y)
+# plt.xlabel('Valence'); plt.ylabel('Arousal')
 
 av_coords = []
 angles = []
 colors = []
 magnitude = []
-for (x, y) in zip(data['valence'], data['arousal']):
-  av_coords.append((x, y))
-  magnitude.append(hypot(x, y))
-  deg = (degrees(atan2(y, x)) + 360) % 360;
-  angles.append(deg)
+
+
+for (v, a) in zip(data['valence'], data['arousal']):
+  if(np.isnan(v) or np.isnan(a)):
+    continue
+  else:
+    av_coords.append((v, a))
+    magnitude.append(hypot(v, a))
+    deg = (degrees(atan2(a, v)) + 360) % 360;
+    angles.append(deg)
 
 for angle in angles:
   colors.append(hsv_to_rgb([angle/360, 1, 1]))
 
 
-
-plt.vlines(x=0, ymin=-1, ymax=1, 
-           colors='red', linewidth=2)
-plt.hlines(y=0, xmin=-1, xmax=1,
-           colors='red', linewidth=2)
+# plt.vlines(x=0, ymin=-1, ymax=1, 
+#            colors='red', linewidth=2)
+# plt.hlines(y=0, xmin=-1, xmax=1,
+#            colors='red', linewidth=2)
 
 # plt.grid(True)
-# x = np.arange(0, )
-
+# x = np.arange(0, len(magnitude))
 # lines = []
 # for i in range(len(x)):
-#     pair=[(x[i],0), (x[i], y[i])]
+#     pair=[(x[i],0), (x[i], magnitude[i])]
 #     lines.append(pair)
 
-# linecoll = matcoll.LineCollection(lines)
+# # linecoll = matcoll.LineCollection(lines)
+# # fig, ax = plt.subplots()
+# # ax.add_collection(linecoll)
 # fig, ax = plt.subplots()
-# ax.add_collection(linecoll)
+# fig.subplots_adjust(bottom=0.2)
+# plt.xticks(rotation=90) 
+# ax.stem(x, magnitude, markerfmt=' ', linefmt=colors)
 
-# plt.scatter(x,y)
+# plt.scatter(x,magnitude,c=colors)
 
 # plt.xticks(x)
 # plt.ylim(0,30)
+x = np.arange(0, len(magnitude))
+(markers, stemlines, baseline) = plt.stem(magnitude, markerfmt=' ')
+plt.setp(stemlines, linestyle="-", color=colors, linewidth=0.5 )
+plt.xticks(np.arange(min(x), max(x)+1, 1), rotation=90)
 
 plt.show()
